dc809c7182ac42be2de38fe48788fb530acbbbb5
edsoncst <48764469+edsoncst@users.noreply.github.com>
2026-05-07 10:46:28 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -19,7 +19,6 @@ const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, {
   cacheSizeBytes: CACHE_SIZE_UNLIMITED,
   experimentalForceLongPolling: true,
-  
 });
 
 export const auth = getAuth(app);
